5170faac0386f6ed5035a9677ca10ad9bd02e531
Belgaux <oscarbelgau@gmail.com>
2017-09-19 18:28:06 +0200

    working binarygemm prototype, with tests

--- bitSerialGEMM/bitSerialGEMM.py
+++ bitSerialGEMM/bitSerialGEMM.py
@@ -1,43 +1,47 @@
 import numpy as np
-import pprint
-from utils import _print, _to_bitstring
+import random
+from utils import _print, to_bitstring, flatten, transpose, \
+    popcount, _and
 
-def _xor(a, b):
-    return a != b
-
-def _and(a, b):
-    return "".join("1" for x,y in zip(a, b)
-            if x == "1" and y == "1")
-
-def popcount(b):
-    return b.count("1")
-
-def gemm(A, W, alpha=None):
-    R = [[0]]*len(W)
-    print(R)
-    for i in range(len(W)):
-        for j in range(len(A[0])):
-            x = W[i][0]
-            y = A[i][j]
-            print(x, y)
-            tmp = _and(x, y)
-            print("tmp:", tmp)
-            R[i][0] += popcount(tmp)
+def binary_gemm(W, A, alpha=None):
+    rows = len(W)
+    cols = len(A)
+    R = [0] * rows
+    for r in range(rows):
+        for c in range(cols):
+            t1 = W[r][c]
+            t2 = A[c]
+            R[r] += popcount(_and(t1, t2))
     return R
-    
 
-def test():
-    a = [[0, 1], [0, 1]]
-    b = [[0], [1]]
-    _print(a, "A")
-    _print(_to_bitstring(a))
-    _print(b, "W")
-    _print(_to_bitstring(b))
+def test_binary_gemm():
+    W = [[0, 0, 0, 1, 1, 1, 0, 0],
+         [1, 0, 0, 1, 1, 0, 0, 1],
+         [0, 0, 1, 0, 1, 1, 1, 1]]
+    A = [0, 1, 1, 1, 1, 1, 1, 0]
+    assert binary_gemm(to_bitstring(W), to_bitstring(A)) \
+            == np.dot(np.asarray(W), np.asarray(A)).tolist()
 
+    W = [[1, 0, 1, 1, 0, 0, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1]]
+    A = [0, 0, 0, 1, 1, 0, 1, 0]
+    assert binary_gemm(to_bitstring(W), to_bitstring(A)) \
+            == np.dot(np.asarray(W), np.asarray(A)).tolist()
 
-    print(gemm(_to_bitstring(a), _to_bitstring(b)))
-    print(np.dot(np.asarray(a), np.asarray(b)))
+def test_random_binary_gemm():
+    for _ in range(100):
+        # random vector
+        A = np.random.randint(0, 2, random.randint(1, 50))
+        # random matrix
+        W = np.random.randint(0, 2, 
+                size=(random.randint(1, 50), A.shape[0]))
+
+        assert np.dot(W, A).tolist() \
+                == binary_gemm(to_bitstring(W.tolist()), \
+                                to_bitstring(A.tolist()))
+
 
 if __name__ == "__main__":
-    test()
+    test_binary_gemm()
+    test_random_binary_gemm()
 
--- bitSerialGEMM/utils.py
+++ bitSerialGEMM/utils.py
@@ -1,29 +1,80 @@
 import pprint
+import itertools
 
 def _print(obj, desc=None):
     if desc:
         print(desc, end=": ")
     pprint.PrettyPrinter().pprint(obj)
 
-def _to_bitstring(M):
+def to_bitstring(M):
+    if isinstance(M, int):
+        # int
+        return bin(M)[2:]
+
     d = __bit_depth(M)
-    return [[__pad_zeroes(bin(e)[2:], d) for e in row] for row in M]
+    if any(isinstance(e, list) for e in M): 
+        # matrix
+        return [[__pad_zeroes(bin(e)[2:], d) for e in row] for row in M]
+    else:
+        # vector
+        return [__pad_zeroes(bin(e)[2:], d) for e in M]
+
+def _xor(a, b):
+    pass
+
+def _and(a, b):
+    """Logical AND for bitstrings """
+    res = []
+    for x, y in itertools.zip_longest(a, b, fillvalue="0"):
+        if x == "1" and y == "1":
+            res.append("1")
+        else:
+            res.append("0")
+    return "".join(res)
+
+
+def transpose(M):
+    return list(map(list, zip(*M)))
 
 def __pad_zeroes(b, n):
     return b.zfill(n)
 
 def __bit_depth(M):
-    return max(len(bin(e)[2:]) for row in M for e in row)
+    if any(isinstance(e, list) for e in M): 
+        # matrix
+        return max(len(bin(e)[2:]) for row in M for e in row)
+    else:
+        # vector
+        return max(len(bin(e)[2:]) for e in M)
+
+def flatten(L):
+    return list(itertools.chain.from_iterable(L))
+
+def popcount(b):
+    return b.count("1")
+
 
 def test_to_bitstring():
     a = [[1, 3], [0, 2]]
-    _print(a)
-    assert _to_bitstring(a) == [["01", "11"], ["00", "10"]]
+    assert to_bitstring(a) == [["01", "11"], ["00", "10"]]
 
     b = [[2], [2]]
-    _print(b)
-    assert _to_bitstring(b) == [["10"], ["10"]]
+    assert to_bitstring(b) == [["10"], ["10"]]
+
+    c = [1,0,0,1,0]
+    assert to_bitstring(c) == ["1", "0", "0", "1", "0"]
+
+def test_and():
+    assert _and("0", "0") == "0"
+    assert _and("0", "1") == "0"
+    assert _and("1", "0") == "0"
+    assert _and("1", "1") == "1"
+
+    for i, j in zip(range(1000, 0, -1), range(0, 1000)):
+        print(i, j)    
+
 
 if __name__ == "__main__":
     test_to_bitstring()
+    test_and()
 
