d06c320d7d7bbdaf4bb746fa08589accb2021316
Belgaux <oscarbelgau@gmail.com>
2017-09-21 14:07:20 +0200

    working bitserial GEMM prototype, for positive numbers

--- bitSerialGEMM/bitserial_gemm.py
+++ bitSerialGEMM/bitserial_gemm.py
@@ -15,7 +15,10 @@ def bitserial_gemm(W, A):
             for r in range(rows):
                 for c in range(cols):
                     tW[r].append(W[r][c][i])
-            alpha = 2**(i+j)
+            # make sure significance is little endian
+            # because we represent bitstrings with LSB first
+            alpha = 2**((len(W[0][0])-i-1)
+                        +(len(A[0])-j-1))
             R = add_lists(R, binary_gemm(tW, tA, alpha))
     return R
 
